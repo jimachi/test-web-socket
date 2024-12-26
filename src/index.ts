1c04869525bbfb38383edc543f1af9b3e5e7ba58
jimachi <y.iijima.dev@gmail.com>
2024-12-26 17:29:58 +0900

    cluster moduleのテスト

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { join } from "node:path";
 import { Server } from "socket.io";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
 let db: import("sqlite").Database;
 
@@ -23,52 +26,64 @@ let db: import("sqlite").Database;
       );
     `);
 
-    const app = express();
-    const server = createServer(app);
-    const io = new Server(server, {
-      connectionStateRecovery: {},
-    });
+    if (cluster.isPrimary) {
+      const numCPUs = availableParallelism();
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+          PORT: 3000 + i,
+        });
+      }
 
-    app.get("/", (req, res) => {
-      res.sendFile(join(__dirname, "index.html"));
-    });
-
-    io.on("connection", async (socket) => {
-      socket.on("chat message", async (msg, clientOffset, callback) => {
-        let result;
-        try {
-          result = await db.run("INSERT INTO messages (content, client_offset) VALUES (?, ?)", msg, clientOffset);
-        } catch (e: any) {
-          if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-            callback();
-          } else {
-            console.error(e);
-          }
-          return;
-        }
-        io.emit("chat message", msg, result.lastID);
-        callback();
+      setupPrimary();
+    } else {
+      const app = express();
+      const server = createServer(app);
+      const io = new Server(server, {
+        connectionStateRecovery: {},
+        adapter: createAdapter(),
       });
 
-      if (!socket.recovered) {
-        try {
-          await db.each(
-            "SELECT id, content FROM messages WHERE id > ?",
-            [socket.handshake.auth.serverOffset || 0],
-            (_err, row) => {
-              socket.emit("chat message", row.content, row.id);
-            }
-          );
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    });
+      app.get("/", (req, res) => {
+        res.sendFile(join(__dirname, "index.html"));
+      });
 
-    const PORT = 3000;
-    server.listen(PORT, () => {
-      console.log("server running at http://localhost:" + PORT);
-    });
+      io.on("connection", async (socket) => {
+        socket.on("chat message", async (msg, clientOffset, callback) => {
+          let result;
+          try {
+            result = await db.run("INSERT INTO messages (content, client_offset) VALUES (?, ?)", msg, clientOffset);
+          } catch (e: any) {
+            if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+              callback();
+            } else {
+              console.error(e);
+            }
+            return;
+          }
+          io.emit("chat message", msg, result.lastID);
+          callback();
+        });
+
+        if (!socket.recovered) {
+          try {
+            await db.each(
+              "SELECT id, content FROM messages WHERE id > ?",
+              [socket.handshake.auth.serverOffset || 0],
+              (_err, row) => {
+                socket.emit("chat message", row.content, row.id);
+              }
+            );
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      });
+
+      const PORT = process.env.PORT;
+      server.listen(PORT, () => {
+        console.log(`server running at http://localhost:${PORT}`);
+      });
+    }
   } catch (error) {
     console.error("Failed to initialize the application:", error);
   }
